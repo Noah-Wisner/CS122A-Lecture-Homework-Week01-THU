d5dcfc2a9ea1eca91cddf33c237e9a0acd118bda
Noah-Wisner <wisnerboy1@gmail.com>
2026-04-03 14:01:22 -0700

    Finished assignment

--- testbench.sv
+++ testbench.sv
@@ -1,18 +1,25 @@
 `timescale 1ns / 1ps
 
-`include "halfadder.sv"
+//`include "halfadder.sv"
 
 module testbench();
 
 // Signals for UUT connection
-reg A;
-reg B;
-wire S;
-wire C;
+reg A_in;
+reg B_in;
+wire S_out;
+wire C_out;
 reg pass;
 integer tests_failed;
 
 // Instantiate the unit under test
+halfadder UUT
+(
+    .a(A_in),
+    .b(B_in),
+    .c(C_out),
+    .s(S_out)
+);
 
 initial begin
     // Set up output to VCDD file
@@ -25,33 +32,47 @@ end
 // Write Test Stimulus
 task test_00();
     begin
-        A = 0;
-        B = 0;
+        A_in = 1'b0;
+        B_in = 1'b0;
         #5;
-        pass &= S == 0 & C == 0;
+        pass &= S_out == 0 & C_out == 0;
     end
 endtask
 
 task test_01();
     begin
         // Put your test for A = 0, and B = 1
+        A_in = 1'b0;
+        B_in = 1'b1;
+        #5;
+        pass &= S_out == 1 & C_out == 0;
     end
 endtask
 
 task test_10();
     begin
-        // Put your test for A = 0, and B = 1
+        // Put your test for A = 1, and B = 0
+        A_in = 1'b1;
+        B_in = 1'b0;
+        #5;
+        pass &= S_out == 1 & C_out == 0;
     end
 endtask
 
 task test_11 ();
     begin
-        // Put your test for A = 0, and B = 1
+        // Put your test for A = 1, and B = 1
+        A_in = 1'b1;
+        B_in = 1'b1;
+        #5;
+        pass &= S_out == 0 & C_out == 1;
     end
 endtask
 
 // Write Checker
 initial begin
+    //initalize pass
+    pass = 1'b1;
 
     test_00(); #15;
     test_01(); #15;
